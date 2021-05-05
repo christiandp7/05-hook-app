f7b6d49bf49b834321e93e550b2907934cf6088a
Christian Diaz Portela <christiandp7@gmail.com>
2021-05-05 11:22:46 -0400

    Test TodoAdd component

---
+++ src/tests/components/08-useReducer/TodoAdd.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoAdd from '../../../components/08-useReducer/TodoAdd'
+
+describe('Puebas en <TodoAdd />', () => {
+	const handleAddTodo = jest.fn()
+	const wrapper = shallow(<TodoAdd handleAddTodo={handleAddTodo} />)
+
+	test('should render correctly', () => {
+		expect(wrapper).toMatchSnapshot()
+	})
+
+	test('should Not call handleAddTodo', () => {
+		const formSubmit = wrapper.find('form').prop('onSubmit') // Makes reference to the function
+		// console.log(formSubmit)
+		formSubmit({ preventDefault() {} })
+
+		expect(handleAddTodo).toHaveBeenCalledTimes(0)
+	})
+
+	test('should call handleAddTodo', () => {
+		const value = 'Aprender Redux'
+		wrapper.find('input').simulate('change', {
+			target: {
+				value,
+				name: 'description',
+			},
+		})
+
+		const formSubmit = wrapper.find('form').prop('onSubmit')
+		formSubmit({ preventDefault() {} })
+		expect(handleAddTodo).toHaveBeenCalledTimes(1)
+		expect(handleAddTodo).toHaveBeenCalledWith(expect.any(Object))
+		expect(handleAddTodo).toHaveBeenCalledWith({
+			id: expect.any(Number),
+			desc: value,
+			done: false,
+		})
+
+		expect(wrapper.find('input').prop('value')).toBe('')
+	})
+})
